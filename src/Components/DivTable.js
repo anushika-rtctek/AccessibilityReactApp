61920264b7e91c178514e7e87036095fdfe48d0f
anushikasng2001 <anushikasng2001@gmail.com>
2024-05-15 12:14:44 +0530

    Accessibility Table using div

--- src/Components/DivTable.js
+++ src/Components/DivTable.js
@@ -1,54 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './DivTable.css'
 
 function DivTable() {
+
+    // const [focusedRow, setFocusedRow] = useState(0)
+    // const [focusedCell, setFocusedCell] = useState(0)
+
+    // useEffect(() => {
+    //     const handleKeyPress = (event) => {
+    //         switch (event.key) {
+    //             case 'ArrowUp':
+    //                 setFocusedRow(prevRow => Math.max(0, prevRow - 1))
+    //                 break
+    //             case 'ArrowDown':
+    //                 setFocusedRow(prevRow => prevRow + 1)
+    //                 break
+    //             case 'ArrowLeft':
+    //                 setFocusedCell(prevCell => Math.max(0, prevCell - 1))
+    //                 break
+    //             case 'ArrowRight':
+    //                 setFocusedCell(prevCell => prevCell + 1)
+    //                 break
+    //             default:
+    //                 break
+    //         }
+    //     }
+    //     window.addEventListener('keydown', handleKeyPress)
+
+    //     return () => {
+    //         window.removeEventListener('keydown', handleKeyPress)
+    //     }
+    // }, [])
+
     return (
-        <div className='Table'>
-            <div className='Title'>
-                <h2>Table using DIV tag</h2>
-            </div>
-            <div className='Heading'>
-                <div className='Cell' itemScope='col'>
-                    Heading 1
+        <div>
+            <h1>Table for reference</h1>
+            <div role='table' aria-label='Test Data'>
+                <div>
+                    <h2>Test Data</h2>
                 </div>
-                <div className='Cell' itemScope='col'>
-                    Heading 2
+                <div role='rowgroup'>
+                    <div role='row'>
+                        <span role='rowheader'>Details</span>
+                        <span role='columnheader'>User 1</span>
+                        <span role='columnheader'>User 2</span>
+                    </div>
                 </div>
-                <div className='Cell' itemScope='col'>
-                    Heading 3
-                </div>
-            </div>
-            <div className='Row'>
-                <div className='Cell'>
-                    Content 1
-                </div>
-                <div className='Cell' itemScope='row'>
-                    Content 2
-                </div>
-                <div className='Cell'>
-                    Content 3
-                </div>
-            </div>
-            <div className='Row'>
-                <div className='Cell'>
-                    Content 1
-                </div>
-                <div className='Cell' itemScope='row'>
-                    Content 2
-                </div>
-                <div className='Cell'>
-                    Content 3
-                </div>
-            </div>
-            <div className='Row'>
-                <div className='Cell'>
-                    Content 1
-                </div>
-                <div className='Cell' itemScope='row'>
-                    Content 2
-                </div>
-                <div className='Cell'>
-                    Content 3
+                <div role='rowgroup'>
+                    <div role='row'>
+                        <span role='rowheader'>Name</span>
+                        <span role='cell'>Anushika</span>
+                        <span role='cell'>Saumya</span>
+                    </div>
+                    <div role='row'>
+                        <span role='rowheader'>Age</span>
+                        <span role='cell'>21</span>
+                        <span role='cell'>24</span>
+                    </div>
+                    <div role='row'>
+                        <span role='rowheader'>City</span>
+                        <span role='cell'>Noida</span>
+                        <span role='cell'>Lucknow</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Components/StyledComp.js
+++
@@ -1,8 +0,0 @@
-// import styled from "styled-components";
-
-// export const StyledDiv = styled.div`
-// display : ${({Table, Row, Cell}) => {Table ? 'table' : Row ? 'table-row' : Cell ? 'table-cell' : ''}};
-// text-align: center;
-// border: ${props => props.Cell ? 'solid thin black' : ''};
-// font-weight: ${props => props.Heading ? 'bold' : ''};
-// padding: ${props => props.Cell ? '10px' : ''};`
