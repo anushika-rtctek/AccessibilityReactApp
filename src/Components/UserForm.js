d06cd7ff1fd9b2607f2d622a0872de1ccdc869e9
anushikasng2001 <anushikasng2001@gmail.com>
2024-06-07 12:23:33 +0530

    Using MUI components in User From

--- src/Components/UserForm.js
+++ src/Components/UserForm.js
@@ -1,85 +1,118 @@
-import { Snackbar } from '@mui/material';
 import React, { useState } from 'react'
-
+import Snackbar from '@mui/material/Snackbar';
+import { Button, FormControlLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField } from '@mui/material';
 
 export const UserForm = () => {
 
-    const [inputs, setInputs] = useState({})
-    const [openSnackbar, setOpenSnackbar] = useState(false);
-    
-    const changeHandler = (event) => {
-        const { name, value, type, checked } = event.target;
-        const newValue = type === 'radio' ? value : value;
-        setInputs(values => ({...values, [name]: newValue}));
-    };
+  const [inputs, setInputs] = useState({})
+  const [open, setOpen] = React.useState(false);
 
-    const submitHandler = event => {
-        event.preventDefault()
-        // alert('Form submitted successfully!!!')
-        setOpenSnackbar(true);
+  const changeHandler = (event) => {
+    const { name, value, type, checked, states } = event.target;
+    const newValue = type === 'radio' ? value : value;
+    setInputs(values => ({ ...values, [name]: newValue }));
+  };
+
+  const submitHandler = event => {
+    event.preventDefault()
+    setOpen(true);
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
     }
+    setOpen(false);
+  };
 
-    const handleCloseSnackbar = () => {
-        setOpenSnackbar(false);
-    };
-
-    const resetHandler = event => {
-        event.preventDefault();
-        setInputs({});
-    }
+  const resetHandler = event => {
+    event.preventDefault();
+    setInputs({});
+  }
 
 
   return (
     <div>
-        <div>
-            <h2>User Form</h2>
-            <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} message="Note archived"/>
-        </div>
+      <div>
+        <h2>User Form</h2>
+        <Snackbar open={open} autoHideDuration={700} onClose={handleClose} message="Form submitted successfully" />
+      </div>
       <form method='post' onSubmit={submitHandler}>
         <fieldset>
-            <legend>Personal Info</legend>
-            <label htmlFor='fname'>First Name</label>
-            <input name='fname' id='fname' onChange={changeHandler} value={inputs.fname || ''} type='text' required/>
-            <label htmlFor='mname'>Middle Name</label>
-            <input name='mname' id='mname' onChange={changeHandler} value={inputs.mname || ''} type='text' required/>
-            <label htmlFor='lname'>Last Name</label>
-            <input name='lname' id='lname' onChange={changeHandler} value={inputs.lname || ''} type='text' required/>
+          <legend>Personal Info</legend>
+          <div style={{ display: 'flex' }}>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='fname'>First Name</InputLabel>
+              <TextField name='fname' id='fname' onChange={changeHandler} value={inputs.fname || ''} type='text' required />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='mname'>Middle Name</InputLabel>
+              <TextField name='mname' id='mname' onChange={changeHandler} value={inputs.mname || ''} type='text' />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='lname'>Last Name</InputLabel>
+              <TextField name='lname' id='lname' onChange={changeHandler} value={inputs.lname || ''} type='text' required />
+            </div>
+          </div>
         </fieldset>
         <fieldset>
-            <legend>Gender</legend>
-            <input name='gender' id='male' onChange={changeHandler} checked={inputs.gender === 'male'} value='male' type='radio'/>
-            <label htmlFor='male'>Male</label>
-            <input name='gender' id='female' onChange={changeHandler} checked={inputs.gender === 'female'} value='female' type='radio'/>
-            <label htmlFor='female'>Female</label>
-            <input name='gender' id='others' onChange={changeHandler} checked={inputs.gender === 'others'} value='others' type='radio'/>
-            <label htmlFor='others'>Others</label>
-            <input name='gender' id='na' onChange={changeHandler} checked={inputs.gender === 'na'} value='na' type='radio'/>
-            <label htmlFor='na'>Prefer not to say</label>
+          <legend>Gender</legend>
+          <RadioGroup>
+            <div style={{ display: 'flex', marginLeft: 30 }}>
+              <FormControlLabel value='male' control={<Radio />} label='Male' />
+              <FormControlLabel value='female' control={<Radio />} label='Female' />
+              <FormControlLabel value='others' control={<Radio />} label='Others' />
+              <FormControlLabel value='na' control={<Radio />} label='Prefer not to specify' />
+            </div>
+          </RadioGroup>
         </fieldset>
         <fieldset>
-            <legend>Contact Info</legend>
-            <label htmlFor='phno'>Phone No.</label>
-            <input name='phno' id='phno' onChange={changeHandler} value={inputs.phno || ''} type='number' maxLength={10} required/>
-            <label htmlFor='email'>Email address</label>
-            <input name='email' id='email' onChange={changeHandler} value={inputs.email || ''} type='email' required/>
+          <legend>Contact Info</legend>
+          <div style={{ display: 'flex' }}>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='dob'>Date of Birth</InputLabel>
+              <TextField name='dob' id='dob' onChange={changeHandler} value={inputs.dob || ''} type='date' maxLength={10} required />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='phno'>Phone No.</InputLabel>
+              <TextField name='phno' id='phno' onChange={changeHandler} value={inputs.phno || ''} type='number' maxLength={10} required />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='email'>Email address</InputLabel>
+              <TextField name='email' id='email' onChange={changeHandler} value={inputs.email || ''} type='email' required />
+            </div>
+          </div>
         </fieldset>
         <fieldset>
-            <legend>Residential Address</legend>
-            <label htmlFor='house'>House/Flat No.and Name </label>
-            <input name='house' id='house' onChange={changeHandler} value={inputs.house || ''} type='text' required/>
-            <label htmlFor='city'>City</label>
-            <input name='city' id='city' onChange={changeHandler} value={inputs.city || ''} type='text' required/>
-            <label htmlFor='states'>State</label>
-            <select name='states' id='states' onChange={changeHandler} value={inputs.states || ''} required>
-                <option value='DL'>Delhi</option>
-                <option value='UP'>Uttar Pradesh</option>
-                <option value='BH'>Bihar</option>
-                <option value='HR'>Haryana</option>
-            </select>
+          <legend>Residential Address</legend>
+          <div style={{ display: 'flex' }}>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='house'>House/Flat No.and Name </InputLabel>
+              <TextField name='house' id='house' onChange={changeHandler} value={inputs.house || ''} type='text' required />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel htmlFor='city'>City</InputLabel>
+              <TextField name='city' id='city' onChange={changeHandler} value={inputs.city || ''} type='text' required />
+            </div>
+            <div style={{ marginLeft: 30 }}>
+              <InputLabel id='state'>State</InputLabel>
+              <Select name='states' id='states' width='100px' labelId='state' label="State" onChange={changeHandler} value={inputs.states || 'NA'} required>
+              <MenuItem value='NA'>Select a State</MenuItem>
+                <MenuItem value='DL'>Delhi</MenuItem>
+                <MenuItem value='UP'>Uttar Pradesh</MenuItem>
+                <MenuItem value='BH'>Bihar</MenuItem>
+                <MenuItem value='HR'>Haryana</MenuItem>
+              </Select>
+            </div>
+          </div>
         </fieldset>
-        <div>
-            <button type='submit'>Submit</button>
-            <button type='reset' onClick={resetHandler}>Reset</button>
+        <div style={{ display: 'flex' }}>
+          <div style={{ marginLeft: 30, marginTop: 30 }}>
+            <Button type='submit' variant='contained'>Submit</Button>
+          </div>
+          <div style={{ marginLeft: 30, marginTop: 30 }}>
+            <Button type='reset' variant='contained' onClick={resetHandler}>Reset</Button>
+          </div>
         </div>
       </form>
     </div>
